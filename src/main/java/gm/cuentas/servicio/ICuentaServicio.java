bcfdc11b968a5bd763724093367513d2b78d6497
efrasss <efrainska5@gmail.com>
2024-04-03 22:50:02 -0600

    Creación Servicio y Repositorio con Spring

---
+++ src/main/java/gm/cuentas/servicio/ICuentaServicio.java
@@ -0,0 +1,15 @@
+package gm.cuentas.servicio;
+
+import gm.cuentas.modelo.Cuenta;
+
+import java.util.List;
+
+public interface ICuentaServicio {
+    public List<Cuenta> listarCuentas();
+
+    public Cuenta buscarCuentaPorId(Integer idCuenta);
+
+    public void guardarCuenta(Cuenta cuenta);
+
+    public void eliminarCuenta(Cuenta contacto);
+}
