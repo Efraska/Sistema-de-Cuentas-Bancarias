8ab59c7b253b78f7d2bd956e7cbbb7f0ed9fc3e4
Efraska <aux_sistemas@medicainsurgentes.com.mx>
2024-04-04 12:28:23 -0600

    Index Controlador con primeFaces/JSF

---
+++ src/main/java/gm/cuentas/controlador/IndexControlador.java
@@ -0,0 +1,36 @@
+package gm.cuentas.controlador;
+
+import gm.cuentas.modelo.Cuenta;
+import gm.cuentas.servicio.CuentaServicio;
+import jakarta.annotation.PostConstruct;
+import jakarta.faces.view.ViewScoped;
+import lombok.Data;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@Data
+@ViewScoped
+public class IndexControlador {
+
+    @Autowired
+    CuentaServicio cuentaServicio;
+    private List<Cuenta> cuentas;
+
+    private static final Logger logger =
+            LoggerFactory.getLogger(IndexControlador.class);
+
+    @PostConstruct
+    public void init(){
+        cargarDatos();
+    }
+
+    public void cargarDatos(){
+        this.cuentas = cuentaServicio.listarCuentas();
+        cuentas.forEach((cuenta) -> logger.info(cuenta.toString()));
+    }
+}
